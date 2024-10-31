0b45587ab16fc57e5d0ac4ce106812b6bf287198
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-11-01 02:57:08 +0300

    Add Custom Bottom Navigation

--- app/src/main/java/com/gamzecoskun/liquidbottomnavigation/ui/MainScreen.kt
+++ app/src/main/java/com/gamzecoskun/liquidbottomnavigation/ui/MainScreen.kt
@@ -1,7 +1,67 @@
 package com.gamzecoskun.liquidbottomnavigation.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gamzecoskun.liquidbottomnavigation.R
+import com.gamzecoskun.liquidbottomnavigation.ui.theme.LiquidBottomNavigationTheme
 
+@Composable
 fun MainScreen() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(bottom = 24.dp)
+    ) {
+        CustomBottomNavigation()
+    }
+}
 
+@Composable
+fun CustomBottomNavigation() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .height(80.dp)
+            .paint(
+                painter = painterResource(R.drawable.bottom_navigation),
+                contentScale = ContentScale.FillHeight
+            )
+            .padding(horizontal = 40.dp)
+    ) {
+        listOf(Icons.Filled.Call, Icons.Filled.Star).map { image ->
+            IconButton(onClick = { }) {
+                Icon(imageVector = image, contentDescription = null, tint = Color.White)
+            }
+        }
+    }
+}
+
+
+
+
+@Composable
+@Preview(device = "id:pixel_4a", showBackground = true, backgroundColor = 0xFF3A2F6E)
+private fun MainScreenPreview() {
+    LiquidBottomNavigationTheme {
+        MainScreen()
+    }
 }
